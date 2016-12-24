4bf7a2a926f97a2682eb39ad8492d8337aa246ad
Andrey Bogdanov <a.bogdanov94@yandex.ru>
2016-12-25 00:46:10 +0300

    Fixed tiles

--- SRC/snesinit.asm
+++ SRC/snesinit.asm
@@ -1,102 +1,262 @@
- .MACRO Snes_Init
- 	sei 	 	; Disabled interrupts
- 	clc 	 	; clear carry to switch to native mode
- 	xce 	 	; Xchange carry & emulation bit. native mode
- 	rep 	#$18 	; Binary mode (decimal mode off), X/Y 16 bit
-         ldx 	#$1FFF  ; set stack to $1FFF
-         txs
- 
-         jsr Init
- .ENDM
- 
- .bank 0
- .section "Snes_Init" SEMIFREE
- Init:
- 	sep 	#$30    ; X,Y,A are 8 bit numbers
- 	lda 	#$8F    ; screen off, full brightness
- 	sta 	$2100   ; brightness + screen enable register 
- 	stz 	$2101   ; Sprite register (size + address in VRAM) 
- 	stz 	$2102   ; Sprite registers (address of sprite memory [OAM])
- 	stz 	$2103   ;    ""                       ""
- 	stz 	$2105   ; Mode 0, = Graphic mode register
- 	stz 	$2106   ; noplanes, no mosaic, = Mosaic register
- 	stz 	$2107   ; Plane 0 map VRAM location
- 	stz 	$2108   ; Plane 1 map VRAM location
- 	stz 	$2109   ; Plane 2 map VRAM location
- 	stz 	$210A   ; Plane 3 map VRAM location
- 	stz 	$210B   ; Plane 0+1 Tile data location
- 	stz 	$210C   ; Plane 2+3 Tile data location
- 	stz 	$210D   ; Plane 0 scroll x (first 8 bits)
- 	stz 	$210D   ; Plane 0 scroll x (last 3 bits) #$0 - #$07ff
- 	lda 	#$FF    ; The top pixel drawn on the screen isn't the top one in the tilemap, it's the one above that.
- 	sta 	$210E   ; Plane 0 scroll y (first 8 bits)
- 	sta 	$2110   ; Plane 1 scroll y (first 8 bits)
- 	sta 	$2112   ; Plane 2 scroll y (first 8 bits)
- 	sta 	$2114   ; Plane 3 scroll y (first 8 bits)
- 	lda 	#$07    ; Since this could get quite annoying, it's better to edit the scrolling registers to fix this.
- 	sta 	$210E   ; Plane 0 scroll y (last 3 bits) #$0 - #$07ff
- 	sta 	$2110   ; Plane 1 scroll y (last 3 bits) #$0 - #$07ff
- 	sta 	$2112   ; Plane 2 scroll y (last 3 bits) #$0 - #$07ff
- 	sta 	$2114   ; Plane 3 scroll y (last 3 bits) #$0 - #$07ff
- 	stz 	$210F   ; Plane 1 scroll x (first 8 bits)
- 	stz 	$210F   ; Plane 1 scroll x (last 3 bits) #$0 - #$07ff
- 	stz 	$2111   ; Plane 2 scroll x (first 8 bits)
- 	stz 	$2111   ; Plane 2 scroll x (last 3 bits) #$0 - #$07ff
- 	stz 	$2113   ; Plane 3 scroll x (first 8 bits)
- 	stz 	$2113   ; Plane 3 scroll x (last 3 bits) #$0 - #$07ff
- 	lda 	#$80    ; increase VRAM address after writing to $2119
- 	sta 	$2115   ; VRAM address increment register
- 	stz 	$2116   ; VRAM address low
- 	stz 	$2117   ; VRAM address high
- 	stz 	$211A   ; Initial Mode 7 setting register
- 	stz 	$211B   ; Mode 7 matrix parameter A register (low)
- 	lda 	#$01
- 	sta 	$211B   ; Mode 7 matrix parameter A register (high)
- 	stz 	$211C   ; Mode 7 matrix parameter B register (low)
- 	stz 	$211C   ; Mode 7 matrix parameter B register (high)
- 	stz 	$211D   ; Mode 7 matrix parameter C register (low)
- 	stz 	$211D   ; Mode 7 matrix parameter C register (high)
- 	stz 	$211E   ; Mode 7 matrix parameter D register (low)
- 	sta 	$211E   ; Mode 7 matrix parameter D register (high)
- 	stz 	$211F   ; Mode 7 center position X register (low)
- 	stz 	$211F   ; Mode 7 center position X register (high)
- 	stz 	$2120   ; Mode 7 center position Y register (low)
- 	stz 	$2120   ; Mode 7 center position Y register (high)
- 	stz 	$2121   ; Color number register ($0-ff)
- 	stz 	$2123   ; BG1 & BG2 Window mask setting register
- 	stz 	$2124   ; BG3 & BG4 Window mask setting register
- 	stz 	$2125   ; OBJ & Color Window mask setting register
- 	stz 	$2126   ; Window 1 left position register
- 	stz 	$2127   ; Window 2 left position register
- 	stz 	$2128   ; Window 3 left position register
- 	stz 	$2129   ; Window 4 left position register
- 	stz 	$212A   ; BG1, BG2, BG3, BG4 Window Logic register
- 	stz 	$212B   ; OBJ, Color Window Logic Register (or,and,xor,xnor)
- 	sta 	$212C   ; Main Screen designation (planes, sprites enable)
- 	stz 	$212D   ; Sub Screen designation
- 	stz 	$212E   ; Window mask for Main Screen
- 	stz 	$212F   ; Window mask for Sub Screen
- 	lda 	#$30
- 	sta 	$2130   ; Color addition & screen addition init setting
- 	stz 	$2131   ; Add/Sub sub designation for screen, sprite, color
- 	lda 	#$E0
- 	sta 	$2132   ; color data for addition/subtraction
- 	stz 	$2133   ; Screen setting (interlace x,y/enable SFX data)
- 	stz 	$4200   ; Enable V-blank, interrupt, Joypad register
- 	lda 	#$FF
- 	sta 	$4201   ; Programmable I/O port
- 	stz 	$4202   ; Multiplicand A
- 	stz 	$4203   ; Multiplier B
- 	stz 	$4204   ; Multiplier C
- 	stz 	$4205   ; Multiplicand C
- 	stz 	$4206   ; Divisor B
- 	stz 	$4207   ; Horizontal Count Timer
- 	stz 	$4208   ; Horizontal Count Timer MSB (most significant bit)
- 	stz 	$4209   ; Vertical Count Timer
- 	stz 	$420A   ; Vertical Count Timer MSB
- 	stz 	$420B   ; General DMA enable (bits 0-7)
- 	stz 	$420C   ; Horizontal DMA (HDMA) enable (bits 0-7)
- 	stz 	$420D	; Access cycle designation (slow/fast rom)
- 	cli 	 	; Enable interrupts
- 	rts
- .ends
+;------------------------------------------------------------------------
+;-  Written by: Neviksti
+;-     If you use my code, please share your creations with me
+;-     as I am always curious :)
+;------------------------------------------------------------------------
+
+
+;----------------------------------------------------------------------------
+; InitSNES -- my "standard" initialization of SNES memory and registers
+;----------------------------------------------------------------------------
+.MACRO InitSNES
+  sei                     ;disable interrupts
+  clc                     ;switch to native mode
+  xce
+
+  REP #$38		; mem/A = 16 bit, X/Y = 16 bit
+			;decimal mode off
+
+  LDX #$1FFF	;Setup the stack
+  TXS			;Transfer Index X to Stack Pointer Register
+
+  ;do the rest of the initialization in a routine
+  JSL $008000
+
+  SEP #$20		; mem/A = 8 bit
+.ENDM
+
+;----------------------------------------------------------------------------
+
+.BANK 0 SLOT 0
+.ORG 0
+.SECTION "InitializeSNESCode" FORCE
+
+InitializeSNES:
+  PHK			;set Data Bank = Program Bank
+  PLB
+
+  LDA #$0000	;set Direct Page = $0000
+  TCD			;Transfer Accumulator to Direct Register
+
+  LDX $1FFD		;we clear all the mem at one point ...
+  STX $4372  	;so save the return address in a place that won't get overwritten
+  LDX $1FFF
+  STX $4374
+
+  SEP #$20		; mem/A = 8 bit
+  REP #$10
+
+  LDA #$8F
+  STA $2100		;turn screen off for now, set brightness to normal
+
+  LDX #$2101
+_Loop00:		;regs $2101-$210C
+  STZ $00,X		;set Sprite,Character,Tile sizes to lowest, and set addresses to $0000
+  INX
+  CPX #$210D
+  BNE _Loop00
+
+_Loop01:		;regs $210D-$2114
+  STZ $00,X		;Set all BG scroll values to $0000
+  STZ $00,X
+  INX
+  CPX #$2115
+  BNE _Loop01
+
+  LDA #$80		;reg $2115
+  STA $2115		; Initialize VRAM transfer mode to word-access, increment by 1
+
+  STZ $2116		;regs $2117-$2117
+  STZ $2117		;VRAM address = $0000
+
+			;reg $2118-$2119
+			;VRAM write register... don't need to initialize
+
+  STZ $211A		;clear Mode7 setting
+
+  LDX #$211B
+_Loop02:		;regs $211B-$2120
+  STZ $00,X		;clear out the Mode7 matrix values
+  STZ $00,X
+  INX
+  CPX #$2121
+  BNE _Loop02
+
+			;reg $2121 - Color address, doesn't need initilaizing
+			;reg $2122 - Color data, is initialized later
+
+  LDX #$2123
+_Loop03:		;regs $2123-$2133
+  STZ $00,X		;turn off windows, main screens, sub screens, color addition,
+  INX			;fixed color = $00, no super-impose (external synchronization),
+  CPX #$2134	;no interlaced mode, normal resolution
+  BNE _Loop03
+
+			;regs $2134-$2136  - multiplication result, no initialization needed
+			;reg $2137 - software H/V latch, no initialization needed
+			;reg $2138 - Sprite data read, no initialization needed
+			;regs $2139-$213A  - VRAM data read, no initialization needed
+			;reg $213B - Color RAM data read, no initialization needed
+			;regs $213C-$213D  - H/V latched data read, no initialization needed
+
+  STZ $213E		;reg $213E - might not be necesary, but selects PPU master/slave mode
+			;reg $213F - PPU status flag, no initialization needed
+
+			;reg $2140-$2143 - APU communication regs, no initialization required
+
+			;reg $2180  -  read/write WRAM register, no initialization required
+			;reg $2181-$2183  -  WRAM address, no initialization required
+
+			;reg $4016-$4017  - serial JoyPad read registers, no need to initialize
+
+
+  STZ $4200		;reg $4200  - disable timers, NMI,and auto-joyread
+
+  LDA #$FF
+  STA $4201		;reg $4201  - programmable I/O write port, initalize to allow reading at in-port
+
+			;regs $4202-$4203  - multiplication registers, no initialization required
+			;regs $4204-$4206  - division registers, no initialization required
+
+			;regs $4207-$4208  - Horizontal-IRQ timer setting, since we disabled this, it is OK to not init
+			;regs $4209-$420A  - Vertical-IRQ timer setting, since we disabled this, it is OK to not init
+
+  STZ $420B		;reg $420B  - turn off all general DMA channels
+  STZ $420C		;reg $420C  - turn off all H-MA channels
+
+  STZ $420D		;reg $420D  - ROM access time to slow (2.68Mhz)
+
+  LDA $4210		;reg $4210  - NMI status, reading resets
+
+			;reg $4211  - IRQ status, no need to initialize
+			;reg $4212  - H/V blank and JoyRead status, no need to initialize
+			;reg $4213  - programmable I/O inport, no need to initialize
+
+			;reg $4214-$4215  - divide results, no need to initialize
+			;reg $4216-$4217  - multiplication or remainder results, no need to initialize
+
+			;regs $4218-$421f  - JoyPad read registers, no need to initialize
+
+			;regs $4300-$437F
+			;no need to intialize because DMA was disabled above
+			;also, we're not sure what all of the registers do, so it is better to leave them at
+			;their reset state value
+
+  JSR ClearVRAM      ;Reset VRAM
+  JSR ClearPalette   ;Reset colors
+
+  ;**** clear Sprite tables ********
+
+  STZ $2102	;sprites initialized to be off the screen, palette 0, character 0
+  STZ $2103
+  LDX #$0080
+  LDA #$F0
+_Loop08:
+  STA $2104	;set X = 240
+  STA $2104	;set Y = 240
+  STZ $2104	;set character = $00
+  STZ $2104	;set priority=0, no flips
+  DEX
+  BNE _Loop08
+
+  LDX #$0020
+_Loop09:
+  STZ $2104		;set size bit=0, x MSB = 0
+  DEX
+  BNE _Loop09
+
+  ;**** clear WRAM ********
+
+  STZ $2181		;set WRAM address to $000000
+  STZ $2182
+  STZ $2183
+
+  LDX #$8008
+  STX $4300         ;Set DMA mode to fixed source, BYTE to $2180
+  LDX #wram_fill_byte
+  STX $4302         ;Set source offset
+  LDA #:wram_fill_byte
+  STA $4304         ;Set source bank
+  LDX #$0000
+  STX $4305         ;Set transfer size to 64k bytes
+  LDA #$01
+  STA $420B         ;Initiate transfer
+
+  LDA #$01          ;now set the next 64k bytes
+  STA $420B         ;Initiate transfer
+
+  PHK			;make sure Data Bank = Program Bank
+  PLB
+
+  CLI			;enable interrupts again
+
+  LDX $4372  	;get our return address...
+  STX $1FFD
+  LDA $4374
+  STA $1FFF
+  RTL
+
+wram_fill_byte:
+.db $00
+
+;----------------------------------------------------------------------------
+; ClearVRAM -- Sets every byte of VRAM to zero
+; In: None
+; Out: None
+; Modifies: flags
+;----------------------------------------------------------------------------
+ClearVRAM:
+   pha
+   phx
+   php
+
+   REP #$30		; mem/A = 8 bit, X/Y = 16 bit
+   SEP #$20
+
+   LDA #$80
+   STA $2115         ;Set VRAM port to word access
+   LDX #$1809
+   STX $4300         ;Set DMA mode to fixed source, WORD to $2118/9
+   LDX #$0000
+   STX $2116         ;Set VRAM port address to $0000
+   STX $0000         ;Set $00:0000 to $0000 (assumes scratchpad ram)
+   STX $4302         ;Set source address to $xx:0000
+   LDA #$00
+   STA $4304         ;Set source bank to $00
+   LDX #$FFFF
+   STX $4305         ;Set transfer size to 64k-1 bytes
+   LDA #$01
+   STA $420B         ;Initiate transfer
+
+   STZ $2119         ;clear the last byte of the VRAM
+
+   plp
+   plx
+   pla
+   RTS
+
+;----------------------------------------------------------------------------
+; ClearPalette -- Reset all palette colors to zero
+; In: None
+; Out: None
+; Modifies: flags
+;----------------------------------------------------------------------------
+ClearPalette:
+   PHX
+   PHP
+   REP #$30		; mem/A = 8 bit, X/Y = 16 bit
+   SEP #$20
+
+   STZ $2121
+   LDX #$0100
+ClearPaletteLoop:
+   STZ $2122
+   STZ $2122
+   DEX
+   BNE ClearPaletteLoop
+
+   PLP
+   PLX
+   RTS
+
+.ENDS
--- SRC/snestest.asm
+++ SRC/snestest.asm
@@ -7,11 +7,36 @@
 .SECTION "MainCode"
 
 Start:
-        Snes_Init            ; Call Snes_Init macro for initialization
+        InitSNES           ; Call macro for initialization
 
         ; Load palette and pattern
         LoadPalette Palette, 0, 16              
-        LoadBlockToVRAM Pattern, $0000, 32
+        LoadBlockToVRAM Pattern, $0000, $0030
+
+        lda #$80
+        sta $2115
+        ; $2116
+        ; vhopppcc cccccccc
+        ; v - vertical flip
+        ; h - horizontal flip
+        ; o - priority bit
+        ; p - palette number (0-7)
+        ; c - location on screen
+
+        ; Using palette #1
+        ; Rendering character at location (0;0)
+        ldx #$0400
+        stx $2116
+        ; $2118 - character number in tile data to put on screen
+        ; Using pattern 2 from tile data
+        lda #$02
+        sta $2118
+        ; Using palette #1 and rendering character at (0;1)
+        ldx #$0401
+        stx $2116
+        ; Using pattern 1 from tile data
+        lda #$01
+        sta $2118
 
         jsr SetupVideo
 
@@ -20,18 +45,27 @@ forever:
         jmp forever
 
 SetupVideo:
-        php                 ; Push P register to stack
+        php
 
         lda #$00
-        sta $2105           ; Set Video mode 0 (8x8 tiles, 4 color BG1/BG2/BG3/BG4)
+        sta $2105           ; Set Video mode 0, 8x8 tiles, 4 color BG1/BG2/BG3/BG4
 
         lda #$04            ; Set BG1's Tile Map offset to $0400 (Word address)
         sta $2107           ; And the Tile Map size to 32x32
 
+        stz $210B           ; Set BG1's Character VRAM offset to $0000 (word address)
+
+        lda #$01            ; Enable BG1
+        sta $212C
+
+        lda #$FF
+        sta $210E
+        sta $210E
+
         lda #$0F
         sta $2100           ; Turn on screen, full Brightness
 
-        plp                 ; Restore P from stack
+        plp
         rts
 
 .ENDS
@@ -43,6 +77,8 @@ SetupVideo:
 Palette:
         .db $00, $00, $FF, $03, $E0, $03, $FF, $7F
 Pattern:
+        .db $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00, $FF, $00
         .db $FF, $FF, $7E, $FE, $3C, $FC, $18, $F8, $18, $F0, $3C, $E0, $7E, $C0, $FF, $80
+        .db $F8, $18, $F0, $3C, $E0, $7E, $C0, $FF, $80, $FF, $FF, $7E, $FE, $3C, $FC, $18
 
 .ENDS
